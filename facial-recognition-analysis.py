2177526920ab33e651cfab8368d12d7e40a768c8
dlam0002 <dlam0002@student.monash.edu>
2021-04-11 20:12:52 +1000

    Import image

--- facial-recognition-analysis.py
+++ facial-recognition-analysis.py
@@ -1,9 +1,17 @@
 import cv2
 import numpy as np
+import matplotlib.pyplot as plt
 from deepface import DeepFace
 
+img = cv2.imread('testfaces\\asian_sad_face.png')
+plt.imshow(img)
+plt.show()
 
-video = cv2.VideoCapture(1)
+
+
+
+'''
+video = cv2.VideoCapture(0)
 
 while True:
     ret, frame = video.read() # ret is a boolean that checks if the camera is on or not, frame is a variable the camera feed is being saved in
@@ -16,6 +24,4 @@ while True:
 
 video.release()
 cv2.destroyAllWindows()
-
-
-
+'''
