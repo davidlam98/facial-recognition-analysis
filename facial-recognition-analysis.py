b64151398abc8a3dde5c1d1ea572ffb2e74db35c
dlam0002 <dlam0002@student.monash.edu>
2021-04-11 19:19:07 +1000

    First commit

---
+++ facial-recognition-analysis.py
@@ -0,0 +1,21 @@
+import cv2
+import numpy as np
+from deepface import DeepFace
+
+
+video = cv2.VideoCapture(0)
+
+while True:
+    ret, frame = video.read() # ret is a boolean that checks if the camera is on or not, frame is a variable the camera feed is being saved in
+    cv2.imshow('frame', frame)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+
+
+
+video.release()
+cv2.destroyAllWindows()
+
+
+
